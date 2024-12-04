ea9d8b0666d90630da5ee8fffeb38ce39e7ef930
Thiago Luiz <47035624+ThiagoLuizS@users.noreply.github.com>
2024-12-04 01:18:23 -0300

    Removendo importes não utilizados e ajustes de validação no DTO CartaoForm

--- src/main/java/br/com/vrbeneficio/configuration/SecurityConfig.java
+++ src/main/java/br/com/vrbeneficio/configuration/SecurityConfig.java
@@ -2,18 +2,15 @@ package br.com.vrbeneficio.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
--- src/main/java/br/com/vrbeneficio/models/dto/form/CartaoForm.java
+++ src/main/java/br/com/vrbeneficio/models/dto/form/CartaoForm.java
@@ -1,8 +1,10 @@
 package br.com.vrbeneficio.models.dto.form;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +22,7 @@ public class CartaoForm {
     @NotNull(message = "Numero do cartão obrigatório")
     private Long numeroCartao;
     @NotEmpty(message = "Senha do cartão obrigatório")
+    @Size(min = 4, max = 8, message = "Informe a senha com no minimo 4 caracteres e 8 no máximo.")
     private String senha;
     @JsonIgnore
     private BigDecimal saldo = BigDecimal.valueOf(500);
